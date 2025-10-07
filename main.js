fa4021561850d74564ee7f3edbd92f1bec9a0ef3
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-10-07 14:41:40 -0300

    07.10

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-const caixaPrincipal = Document.querySelector(".caixa-principal");
-const caixaPerguntas = Document.querySelector(".caixa-perguntas");
-const caixaAlternativas = Document.querySelector(".caixa-alternativas");
-const caixaResultado = Document.querySelector(".caixa-resultado");
-const textoResultado = Document.querySelector(".texto-resultado");
-
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 
 const perguntas = [
